666d080153dce0097215f2b27af2f53a6642dc7b
Senna Parsa <sennap@users.noreply.github.com>
2020-10-26 13:02:32 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+| Linux | Windows | MacOS|
+|-------|-------|-------|
+| ![](https://github.com/rebelware/fibonacci-generator/workflows/node-ci/badge.svg)  | ![](https://github.com/rebelware/fibonacci-generator/workflows/node-ci/badge.svg) | ![](https://github.com/rebelware/fibonacci-generator/workflows/node-ci/badge.svg) |
+# fib-tools
+
+## Get the nth Number
+
+```javascript
+const {getNumber} = require('@bbq-beets/fib-tools')
+assert.strictEqual(getNumber(8), 21)
+```
+
+## Get a List of Numbers
+
+```javascript
+const {getList} = require('@bbq-beets/fib-tools')
+assert.strictDeepEqual(getList(8), [0, 1, 1, 2, 3, 5, 8, 13, 21])
+```
+
+## Get a Sequence of Numbers   
+
+```javascript   
+const {getSequence} = require('@bbq-beets/fib-tools')
+
+const seq = getSequence()
+
+for (const n of seq) {
+  console.log(n) // The next Fibonacci number in the sequence
+}
+```
---
+++ bootstrap-5-min-demo.sh
@@ -0,0 +1,53 @@
+
+#!/usr/bin/env bash
+
+function usage {
+  echo
+  echo
+  echo "USAGE: bootstrap-5-min-demo [GitHub PAT] [-h]"
+  echo "    -h Print this message"
+  echo "    If no PAT is provided, we'll try to use the value of the TOKEN env var"
+  echo 
+  echo 
+}
+
+if [[ $1 = '-h' ]]; then
+  echo "case 1"
+  usage
+  exit 1
+elif [[ $1 != "" ]]; then
+  echo "case 2"
+  PAT=$1
+elif [[ ! -z "$TOKEN" ]]; then
+  echo "case 3"
+  PAT=$TOKEN
+else
+  echo 
+  echo "ERROR: No PAT provided or set in \$TOKEN"
+  usage
+  exit 0
+fi
+
+Echo "PAT: ${PAT}"
+ curl --location --request DELETE 'https://api.github.com/repos/rebelware/fibonacci-generator' \
+ --header "Authorization: token ${PAT}" \
+ --header 'Accept: application/vnd.github.v3+json' \
+ --header 'Content-Type: application/json' \
+ --header 'Cookie: _octo=GH1.1.2138110587.1575167418; logged_in=no' \
+ --data-raw ''
+
+ curl --location --request POST 'https://api.github.com/orgs/rebelware/repos' \
+--header "Authorization: token ${PAT}" \
+ --header 'Accept: application/vnd.github.v3+json' \
+ --header 'Content-Type: application/json' \
+ --header 'Cookie: _octo=GH1.1.2138110587.1575167418; logged_in=no' \
+ --data-raw '{
+   "name":"fibonacci-generator"
+   ,"visibility":"private"
+   ,"private":true
+ }'
+
+rm -rf .git
+git init
+git remote add origin git@github.com:rebelware/fibonacci-generator.git
+
